2cca09f6b3c173bfea74368c4b64bcc7dfe85eb7
Pierce Petit <ppetit2496@gmail.com>
2022-05-30 19:35:55 -0500

    complete task 1-3

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
-    prevGuesses: []
+    prevGuesses: [] // adds prev guess to obj
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     }
 }
+getGuess: function () { // getting guess meth to gane 
+    let guess;
+    do {
+        guess = parseInt(// convert string to integer
+            prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+        )
+    }
+}
+while (isNaN(guess)|| guess < this.smallestNum || guess > this.biggestNum) { //making sure its a number and in the range
+    return guess 
+},
